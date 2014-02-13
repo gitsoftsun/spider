2d9a32b9b98fc0701c057a0e5df3711055e44312
mike442144 <mike442144@163.com>
2014-02-13 09:32:53 +0800

    no continue

--- fake_request.js
+++ fake_request.js
@@ -1,6 +1,8 @@
 var fs = require('fs')
 var readline = require('readline');
 var StringBuilder = require('stringbuilder')
+var http = require('http')
+var zlib = require('zlib')
 // var cities={};
 // fs.readFileSync('TextFile1.txt').toString().split("\r\n").forEach(function(line){
 // 	var pyh = line.split(' ');
@@ -17,6 +19,107 @@ var StringBuilder = require('stringbuilder')
 // sb.build(function(err,result){
 // 	console.log(result);
 // });
+var hotel_detail_options={
+    host:'m.ctrip.com',
+    path:'/html5/Hotel/GetHotelDetail',
+    port:80,
+    method:'POST',
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch)',
+        "X-Requested-With":"XMLHttpRequest",
+        "Accept":"application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding":"gzip, deflate",
+        "Accept-Language":"en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3",
+        "Content-Type":"application/json",
+        "X_FORWARDED_FOR":"58.99.128.66",
+    }
+};
+var requestDetailData = function(cityId,hotelId){
+    this.CheckInDate= "2014-02-23";
+    this.CheckOutDate="2014-02-24";
+    this.CityID= cityId;
+    this.HotelID= hotelId;
+    this.IsMorning= "0";
+};
+
+var rd = new requestDetailData("2",425161);
+get_res_data(rd,function(obj){
+	obj = obj&&obj.Data;
+	obj = JSON.CtripUnPack(obj);
+	console.log(obj[0].RoomDetailList);
+},"detail");
+
+JSON.CtripUnPack = function(e) {
+    var q = "return o;";
+    for (var u = [], f = [], i = e[0], n = i.length, l = e.length, x = -1, A = -1, a = 0, p = 0, d, o; a < n; ++a) {
+        f[++A] = i[a];
+        if (typeof i[a + 1] == "object") {
+            ++a;
+            for (d = 1; d < l; ++d) {
+                o = e[d];
+                o[p] = i[a][o[p]]
+            }
+        }
+        ++p
+    }
+    for (var k = 1; k < l; ++k)
+        for (var g = e[k], c = 0; c < f.length; c++)
+            if (typeof g[c] == "object")
+                if (g[c] && g[c].length > 0) {
+                    for (var t = [], h = [], s = g[c][0], m = s.length, v = g[c].length, w = -1, y = -1, b = 0, z = 0, j; b < m; ++b) {
+                        h[++y] = s[b];
+                        ++z
+                    }
+                    for (b = 0, m = h.length; b < m; ++b)
+                        h[b] = 'o["'.concat(h[b].replace('"', "\\x22"), '"]=a[', b, "];");
+                    var r = Function("o,a", h.join("") + q);
+                    for (j = 1; j < v; ++j)
+                        t[++w] = r({}, g[c][j]);
+                    e[k][c] = t
+                }
+    for (a = 0, n = f.length; a < n; ++a)
+        f[a] = 'o["'.concat(f[a].replace('"', "\\x22"), '"]=a[', a, "];");
+    var r = Function("o,a", f.join("") + q);
+    for (d = 1; d < l; ++d)
+        u[++x] = r({}, e[d]);
+    return u
+};
+function get_res_data(json_data,fn,type){
+    var strData = JSON.stringify(json_data);
+    var options=null;
+    if(type=="detail"){
+    	hotel_detail_options.headers['Content-Length'] = Buffer.byteLength(strData,'utf8');
+        options=hotel_detail_options;
+        
+    }
+    else{
+        hotel_list_options.headers['Content-Length'] = Buffer.byteLength(strData,'utf8');
+        options=hotel_list_options;
+    }
+        
+    var req = http.request(options, function(res) {
+	//res.setEncoding('utf8');
+	var chunks=[];
+	res.on('data', function (chunk) {
+	    chunks.push(chunk);
+	});
+	res.on('end',function(){
+  	    if(res.headers['content-encoding']=='gzip'){
+  		var buffer = Buffer.concat(chunks);
+  		zlib.gunzip(buffer,function(err,decoded){
+		    if(decoded){
+  			fn(JSON.parse(decoded.toString()),json_data['CheckInCityID']);
+		    }
+  		});
+	    }
+	});
+    });
+    req.on('error', function(e) {
+	console.log('problem with request: ' + e.message);
+    });
+    req.write(strData);
+    req.end();
+}
 
 var hotels=[];
 var hotel=function(){
@@ -57,47 +160,47 @@ var room = function(){
 var pageCount=1;
 var curPageIdx=1;
 
-var file = fs.readFile("response_format.json","utf8",function(err,data){
-	if(err)
-		return console.log(err);
-	var obj = JSON.parse(data);
-	var fileds = obj.Data[0];// array 
-	var data_arr = obj.Data[1];
-	var hotelList = data_arr[0];
-	var totalCount = data_arr[1];
-	pageCount = Number(data_arr[2]);
-	var currentPos = data_arr[3];
-	var isInterested = data_arr[4];
-	for(var i=0;i<hotelList.length;i++){
-	    var h = new hotel();
-	    var h_arr = hotelList[i];
-	    h.id=h_arr[0];
-	    h.name=h_arr[2];
-	    h.shortName=h_arr[3];
-	    h.star = h_arr[5];
-	    h.currency=h_arr[6];
-	    h.lowPrice=h_arr[7];
-	    h.points = h_arr[8];
-	    h.zoneName = h_arr[9];
-	    h.isGift = h_arr[10]==1?true:false;
-	    h.isNew = h_arr[11]==1?true:false;
-	    h.isFan = h_arr[12]==1?true:false;
-	    h.fanPrice=h_arr[13];
-	    h.isQuan = h_arr[14]==1?true:false;
-	    h.quanPrice = h_arr[15];
-	    h.quanType = h_arr[16];
-	    h.isCu = h_arr[17]==1?true:false;
-	    h.isMp=h_arr[18]==1?true:false;
-	    h.isMorning=h_arr[19]==1?true:false;
-	    h.isStar = h_arr[20]==1?true:false;
-	    hotels.push(h);
-	}
+// var file = fs.readFile("response_format.json","utf8",function(err,data){
+// 	if(err)
+// 		return console.log(err);
+// 	var obj = JSON.parse(data);
+// 	var fileds = obj.Data[0];// array 
+// 	var data_arr = obj.Data[1];
+// 	var hotelList = data_arr[0];
+// 	var totalCount = data_arr[1];
+// 	pageCount = Number(data_arr[2]);
+// 	var currentPos = data_arr[3];
+// 	var isInterested = data_arr[4];
+// 	for(var i=0;i<hotelList.length;i++){
+// 	    var h = new hotel();
+// 	    var h_arr = hotelList[i];
+// 	    h.id=h_arr[0];
+// 	    h.name=h_arr[2];
+// 	    h.shortName=h_arr[3];
+// 	    h.star = h_arr[5];
+// 	    h.currency=h_arr[6];
+// 	    h.lowPrice=h_arr[7];
+// 	    h.points = h_arr[8];
+// 	    h.zoneName = h_arr[9];
+// 	    h.isGift = h_arr[10]==1?true:false;
+// 	    h.isNew = h_arr[11]==1?true:false;
+// 	    h.isFan = h_arr[12]==1?true:false;
+// 	    h.fanPrice=h_arr[13];
+// 	    h.isQuan = h_arr[14]==1?true:false;
+// 	    h.quanPrice = h_arr[15];
+// 	    h.quanType = h_arr[16];
+// 	    h.isCu = h_arr[17]==1?true:false;
+// 	    h.isMp=h_arr[18]==1?true:false;
+// 	    h.isMorning=h_arr[19]==1?true:false;
+// 	    h.isStar = h_arr[20]==1?true:false;
+// 	    hotels.push(h);
+// 	}
 	
-	console.log(hotels.length);//HotelLists
-	console.log(totalCount);//TotalCount
-//	console.log(totalPage);//TotalPage
-	console.log(currentPos);//currentPos
-	console.log(isInterested);//isInterested
+// 	console.log(hotels.length);//HotelLists
+// 	console.log(totalCount);//TotalCount
+// //	console.log(totalPage);//TotalPage
+// 	console.log(currentPos);//currentPos
+// 	console.log(isInterested);//isInterested
 
     
-});
+// });
--- require.js
+++ require.js
@@ -55,13 +55,15 @@ requestData.clone=function(){
     return result;
 };
 
-var requestDetailData = {
-    "CheckInDate": "2014-02-13"
-    "CheckOutDate": "2014-02-14"
-    "CityID": "2"
-    "HotelID": "425179"
-    "IsMorning": "0"
+var requestDetailData = function(cityId,hotelId){
+    this.CheckInDate= "2014-02-23";
+    this.CheckOutDate="2014-02-24";
+    this.CityID= cityId;
+    this.HotelID= hotelId;
+    this.IsMorning= "0";
 };
+
+
 //get cities from file
 
 var cities={};
@@ -69,6 +71,8 @@ var doneCities={};
 var curCity=0;
 var doneCityCount=0;
 var cityCount=0;
+var doneHotelCount=0;
+var hotelCount=0;
 fs.readFileSync('TextFile1.txt').toString().split("\r\n").forEach(function(line){
     var pyh = line.split(' ');
     var id = pyh[0].match(/\d+/);
@@ -79,9 +83,9 @@ fs.readFileSync('TextFile1.txt').toString().split("\r\n").forEach(function(line)
 
 var pageCount=1;
 var curPageIdx=1;
-var hotels=[];
+var hotels={};
 var hotel=function(){
-    this.city=0;
+    this.city="";
     this.id = 0;
     this.name = "";
     this.shortName = "";
@@ -90,7 +94,7 @@ var hotel=function(){
     this.lowPrice="";
     this.points="";
     this.zoneName="";
-
+    this.picCount=0;
     this.commentCount=0;
 
     this.isGift=false;
@@ -107,20 +111,32 @@ var hotel=function(){
     this.isStar=false;
     this.isRoomFull=false;
 
+    
+    this.faclPoints= "0";//设施
+    this.raAtPoints = "0";//环境
+    this.ratPoints = "0";//卫生
+    this.servPoints = "0";//服务
     this.rooms = [];
 };
 var room = function(){
     this.id=0;
     this.name="";
-    this.price=0;
     this.breakfast="";
+    this.fan="";
+    this.gift="";
+    this.isCu=0;
+    this.payType=1;
+    this.price="";
 };
 hotel.prototype.appendToFile=function(){
+    //console.log("hotel "+this.id+" room count: "+this.rooms.length);
     var sb = new StringBuilder({newline:'\r\n'});
     for(var i=0;i<this.rooms.length;i++){
         sb.append(this.city);
         sb.append(",");
         sb.append(this.name);
+        sb.append(",");
+        sb.append(this.zoneName==null?"":this.zoneName);
         sb.append(",");
         sb.append(this.star);
         sb.append(",");
@@ -130,24 +146,52 @@ hotel.prototype.appendToFile=function(){
         sb.append(",");
         sb.append(this.commentCount);
         sb.append(",");
+        sb.append(this.picCount);
+        sb.append(",");
         sb.append(this.points);
         sb.append(",");
-        sb.append(this.rooms[i].breakfast);
+        sb.append(this.faclPoints);
         sb.append(",");
-        sb.append(this.isGift);
+        sb.append(this.raAtPoints);
         sb.append(",");
-        sb.append(this.isCu);
+        sb.append(this.servPoints);
         sb.append(",");
-        sb.append(this.isFan&&this.fanPrice);
+        sb.append(this.ratPoints);
+        sb.append(",");
+        var b;
+        if(this.rooms[i].breakfast=="单早")
+            b=1;
+        else if(this.rooms[i].breakfast=="双早")
+            b=2;
+        else b=0;
+        sb.append(b);
+        sb.append(",");
+        if(this.rooms[i].gift)
+            sb.append(this.rooms[i].gift);
+        else sb.append("");
+        sb.append(",");
+        sb.append(this.isCu==0?"N":"Y");
+        sb.append(",");
+        if(this.rooms[i].fanPrice)
+            sb.append(this.rooms[i].fanPrice);
+        else sb.append("");
+        sb.append(",");
+        sb.append(this.rooms[i].payType==0?"Y":"N");
         sb.append('\r\n');
     }
-    sb.append(this.city+","+this.name+","+this.star+","+this.isCu+"\r\n");
+    var id=this.id;
+//    sb.append(this.city+","+this.name+","+this.star+","+this.isCu+"\r\n");
     sb.build(function(err,result){
         if(err) {
             console.log(err);
             return;
         }
-        fs.appendFileSync("data.txt",result);
+        //console.log("writing "+id+"...");
+        fs.appendFile("data.txt",result,function(e){
+            if(e) console.log(e);
+            console.log(++doneHotelCount+"/"+hotelCount);
+            if(doneHotelCount==hotelCount) console.log("job complete.\r\n");
+        });
     });
     
 }
@@ -161,56 +205,88 @@ for(var k in cities){
     c["rd"]["PageNumber"]=1;
     console.log("getting "+c.name+"...")
     get_res_data(c["rd"],one_page_data);
-    doneCities[k]=true;
+    
 }
 
 function hotel_page_data(obj){
-
+    var data=null;
+    if(obj&&obj.Data){
+        data = obj.Data;
+    }
+    if(data&&data!==''){
+        data = JSON.CtripUnPack(data);
+        if(data&&data[0]){
+            var h = hotels[data[0].HotelID];
+            
+            h.commentCount=data[0].CommentTotal;
+            h.custPoints = data[0].CustPoints;
+            h.faclPoints= data[0].FaclPoints;//设施
+            h.raAtPoints = data[0].RaAtPoints;//环境
+            h.ratPoints = data[0].RatPoints;//卫生
+            h.servPoints = data[0].ServPoints;//服务
+            if(data[0].HotelPicList)
+                h.picCount = data[0].HotelPicList.length;
+            if(data[0].RoomDetailList){
+                for(var i in data[0].RoomDetailList){
+                    var r = data[0].RoomDetailList[i];
+                    var rm = new room();
+                    rm.id=r.RoomID;
+                    rm.name=r.RoomName;
+                    rm.price=r.RoomPrice;
+                    rm.breakfast = r.Breakfast;
+                    rm.fan = r.FanDesc;
+                    rm.gift = r.GiftName;
+                    rm.isCu = r.IsCu;
+                    rm.payType = r.PayType;
+                    h.rooms.push(rm);
+                }
+                h.appendToFile();
+            }
+        }
+    }
 }
 
 function one_page_data(obj,cityId){
-    var fileds = obj&&obj.Data&&obj.Data[0];// array 
-    var data_arr = obj&&obj.Data&&obj.Data[1];
-    if(!data_arr) return;
-    var hotelList = data_arr[0];
-    var totalCount = data_arr[1];
-    cities[cityId].pageCount = data_arr[2];
-    var currentPos = data_arr[3];
-    var isInterested = data_arr[4];
-    for(var i=0;i<hotelList.length;i++){
-	var h = new hotel();
-	var h_arr = hotelList[i];
-    h.city=cities[cityId]&&cities[cityId].name;
-	h.id=h_arr[0];
-	h.name=h_arr[2];
-	h.shortName=h_arr[3];
-	h.star = h_arr[5];
-	h.currency=h_arr[6];
-	h.lowPrice=h_arr[7];
-	h.points = h_arr[8];
-	h.zoneName = h_arr[9];
-	h.isGift = h_arr[10]==1?true:false;
-	h.isNew = h_arr[11]==1?true:false;
-	h.isFan = h_arr[12]==1?true:false;
-	h.fanPrice=h_arr[13];
-	h.isQuan = h_arr[14]==1?true:false;
-	h.quanPrice = h_arr[15];
-	h.quanType = h_arr[16];
-	h.isCu = h_arr[17]==1?true:false;
-	h.isMp=h_arr[18]==1?true:false;
-	h.isMorning=h_arr[19]==1?true:false;
-	h.isStar = h_arr[20]==1?true:false;
-	hotels.push(h);
+    if (!obj || (obj.ServerCode != 1 && obj.ServerCode != 6)){
+        return;
     }
-    //console.log(cities[cityId].name+" total page count:"+cities[cityId].pageCount);
-    if(cities[cityId].curPageIdx==cities[cityId].pageCount) {
-        doneCityCount++;
-        if(doneCityCount==cityCount){
-            //saveFile();
+    if(obj.Data){
+        var a = obj.Data;
+        a = JSON.CtripUnPack(a);
+        if(a && a.length > 0 && a[0].TotalCount > 0 && a[0].HotelLists && a[0].HotelLists.length > 0){
+            cities[cityId].pageCount=a[0].TotalPage;
+            for(var i=0;i<a[0].HotelLists.length;i++){
+                var h = new hotel();
+                var h_obj = a[0].HotelLists[i];
+                h.city=cities[cityId]&&cities[cityId].name;
+                h.id=h_obj.HotelID;
+                h.name=h_obj.HotelName;
+                //h.shortName=h_arr[3];
+                h.star = h_obj.Star;
+                h.currency=h_obj.Currency;
+                //h.lowPrice=h_arr[7];
+                h.points = h_obj.Points;
+                h.zoneName = h_obj.ZoneName;
+                //h.isGift = h_arr[10]==1?true:false;
+                //h.isNew = h_arr[11]==1?true:false;
+                //h.isFan = h_arr[12]==1?true:false;
+                //h.fanPrice=h_arr[13];
+                //h.isQuan = h_arr[14]==1?true:false;
+                //h.quanPrice = h_arr[15];
+                //h.quanType = h_arr[16];
+                //h.isCu = h_arr[17]==1?true:false;
+                //h.isMp=h_arr[18]==1?true:false;
+                //h.isMorning=h_arr[19]==1?true:false;
+                //h.isStar = h_arr[20]==1?true:false;
+                hotels[h.id]=h;
+                hotelCount++;
+                get_res_data(new requestDetailData(cityId,h.id),hotel_page_data,"detail");
+            }
+            console.log("got "+cities[cityId].curPageIdx+++"/"+cities[cityId].pageCount);    
         }
     }
-
-    console.log("got "+cities[cityId].curPageIdx+++"/"+cities[cityId].pageCount);
+    else console.log("failed "+ cities[cityId].curPageIdx+++"/"+cities[cityId].pageCount);
+        
     while(cities[cityId]["rd"]["PageNumber"]<cities[cityId].pageCount){
         //++cities[cityId].curPageIdx;
         cities[cityId]["rd"]["PageNumber"]++;
@@ -218,51 +294,78 @@ function one_page_data(obj,cityId){
     }
 }
 
-function saveFile(){
-    console.log("starting saving files...");
-    hotels.forEach(function(h){
-        h.appendToFile();
-    });
-}
-
-function get_res_data(json_data,fn,type="list"){
+function get_res_data(json_data,fn,type){
     var strData = JSON.stringify(json_data);
     var options=null;
-    if(type=="list"){
+    if(type=="detail"){
+        hotel_detail_options.headers['Content-Length'] = Buffer.byteLength(strData,'utf8');
+        options=hotel_detail_options;
+        
+    }
+    else{
         hotel_list_options.headers['Content-Length'] = Buffer.byteLength(strData,'utf8');
         options=hotel_list_options;
     }
         
-    else{
-        hotel_detail_options.headers['Content-Length'] = Buffer.byteLength(strData,'utf8');
-        options=hotel_detail_options;
-    }
-        
     var req = http.request(options, function(res) {
-	//res.setEncoding('utf8');
-	var chunks=[];
-	res.on('data', function (chunk) {
-	    chunks.push(chunk);
-	});
-	res.on('end',function(){
-  	    if(res.headers['content-encoding']=='gzip'){
-  		var buffer = Buffer.concat(chunks);
-  		zlib.gunzip(buffer,function(err,decoded){
-		    if(decoded){
-  			fn(JSON.parse(decoded.toString()),json_data['CheckInCityID']);
-		    }
-  		});
-	    }
-	});
+    //res.setEncoding('utf8');
+    var chunks=[];
+    res.on('data', function (chunk) {
+        chunks.push(chunk);
+    });
+    res.on('end',function(){
+        if(res.headers['content-encoding']=='gzip'){
+        var buffer = Buffer.concat(chunks);
+        zlib.gunzip(buffer,function(err,decoded){
+            if(decoded){
+            fn(JSON.parse(decoded.toString()),json_data['CheckInCityID']);
+            }
+        });
+        }
+    });
     });
     req.on('error', function(e) {
-	console.log('problem with request: ' + e.message);
+    console.log('problem with request: ' + e.message);
     });
     req.write(strData);
     req.end();
 }
 
-
+JSON.CtripUnPack = function(e) {
+    var q = "return o;";
+    for (var u = [], f = [], i = e[0], n = i.length, l = e.length, x = -1, A = -1, a = 0, p = 0, d, o; a < n; ++a) {
+        f[++A] = i[a];
+        if (typeof i[a + 1] == "object") {
+            ++a;
+            for (d = 1; d < l; ++d) {
+                o = e[d];
+                o[p] = i[a][o[p]]
+            }
+        }
+        ++p
+    }
+    for (var k = 1; k < l; ++k)
+        for (var g = e[k], c = 0; c < f.length; c++)
+            if (typeof g[c] == "object")
+                if (g[c] && g[c].length > 0) {
+                    for (var t = [], h = [], s = g[c][0], m = s.length, v = g[c].length, w = -1, y = -1, b = 0, z = 0, j; b < m; ++b) {
+                        h[++y] = s[b];
+                        ++z
+                    }
+                    for (b = 0, m = h.length; b < m; ++b)
+                        h[b] = 'o["'.concat(h[b].replace('"', "\\x22"), '"]=a[', b, "];");
+                    var r = Function("o,a", h.join("") + q);
+                    for (j = 1; j < v; ++j)
+                        t[++w] = r({}, g[c][j]);
+                    e[k][c] = t
+                }
+    for (a = 0, n = f.length; a < n; ++a)
+        f[a] = 'o["'.concat(f[a].replace('"', "\\x22"), '"]=a[', a, "];");
+    var r = Function("o,a", f.join("") + q);
+    for (d = 1; d < l; ++d)
+        u[++x] = r({}, e[d]);
+    return u
+};
 
 
 var stdin = process.openStdin();
