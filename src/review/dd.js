4970048be239056436b79cd2b8b3ac75c027e5d2
ltloveandroid <ltloveandroid@gmail.com>
2014-10-13 14:29:42 +0800

    dd html parse complete, to be debug

--- src/review/dd.js
+++ src/review/dd.js
@@ -15,14 +15,12 @@ function DD(){
     this.interval = [0,500];
     this.doneItems = {};
     //could use pagination query parameters directly.
-    this.query = function(cateId,cateName,cateCode,cityName,cityCode,pageIdx){
+    this.query = function(ajax, offset, areaid, areaseo){
 	//this.categoryId=296;
-	this.categoryId=cateId
-	this.what=cateName;//"Ng%C3%A2n+h%C3%A0ng"
-	this.whatUrl=cateCode;//"ngan-hang-c296"
-	this.where=cityName;//"H%C3%A0+N%E1%BB%99i"
-	this.whereUrl=cityCode;//"ha-noi"
-	this.page=pageIdx || 1;
+    this.ajax = ajax || 1;
+    this.offset = offset || 0;
+    this.areaid = areaid;
+    this.areaseo = areaseo;
 	this.sort = "";
     }
 }
@@ -42,7 +40,7 @@ DD.prototype.init = function(){
 	return line;
     }).map(function(line){
 	var vals = line.split(',');
-	return {enname:vals[0],code:vals[1],name:vals[2]};
+	return {enname:vals[0],code:vals[1],seo:vals[2],name:vals[3]};
     });
     /*this.categories = fs.readFileSync(this.dataDir+this.categoryFile).toString().split('\n').filter(function(line){
 	return line;
@@ -95,16 +93,16 @@ DD.prototype.start = function(){
 DD.prototype.wgetList = function(t){
     if(!t){
 	t = this.taskQueue.shift();
-	t.pageIdx = 1;
+	t.offset = 0;
 	t.shopCount=-1;
 	t.reviews = 0;
 	t.shops = [];
 	t.photoCount = 0;
     }
-    var q = new this.query("",t.pageIdx);//I don't know the query parameters for pool network
-    var opt = new helper.basic_options('diadiemanuong.com/',"/",'POST',false,true,q);//path of url the same to query parameters.
+    var q = new this.query(1, t.offset, t.city.code, t.city.seo);//I don't know the query parameters for pool network
+    var opt = new helper.basic_options('diadiemanuong.com/',"/location/ajaxLoadMore",'POST',false,true,q);//path of url the same to query parameters.
     console.log(opt);
-    console.log("[GET] %s: %d",t.city.enname,t.pageIdx);
+    console.log("[GET] %s: %d + (10 more)",t.city.enname,t.offset);
     helper.request_data(opt,q,function(data,args,res){
 	that.processList(data,args,res);
     },t);
@@ -123,10 +121,17 @@ DD.prototype.processList = function(data,args,res){
     
     var $ = cheerio.load(data);
     var itemSelector;
-    itemSelector = "";//shop item selector
+    itemSelector = "li.detail-event-main-item";//shop item selector
     $(itemSelector).each(function(){
 	var shop = {};
-	//TODO: parse elements to shop.
+    //TODO: parse elements to shop.
+    var item = $("div.desc h2.post-title a", this);
+    shop.path = item.attr("href");
+    shop.path = shop.path.replace("http://diadiemanuong.com", "");
+    shop.name = item.attr("title");
+    shop.star = Number(5);//always 5, so it's nonsense
+    shop.like = Number($("div.desc p.place-action span.like-count", this).text());
+    shop.reviews = Number($("div.desc p.place-action span.comment-count", this).text());
 	args[0].shops.push(shop);
     });
     console.log("[DATA] %s, %d",args[0].city.enname,args[0].pageIdx);
@@ -135,7 +140,7 @@ DD.prototype.processList = function(data,args,res){
 
 DD.prototype.wgetDetail = function(task){
     if(task.shops.length==0){
-	++task.pageIdx;
+	task.offset += 10;
 	this.wgetList(task);
 	return;
     }
@@ -156,8 +161,11 @@ DD.prototype.processDetail = function(data,args){
     //TODO: photoCount and category info.
     //photoCount is on the bubble of top right.
     //category is on the right of the map which tag color is yellow.
+    args[1].photoCount = $("div.review-widget-content div div.thread_image_list img").length;
+    var category_item = $("div#place-detail-desc div.desc a", this);
+    args[1].category = category_item.attr("title");
     
-    var record = [args[0].city.enname,args[1].cateogry,args[1].path,args[1].star,args[1].reviews,args[1].photoCount,'\n'].join();
+    var record = [args[0].city.enname,args[1].path,args[1].star,args[1].like,args[1].reviews,args[1].photoCount,args[1].category,'\n'].join();
     fs.appendFileSync(this.resultDir+this.resultFile,record);
     console.log("[DONE] %s",record);
     setTimeout(function(){
