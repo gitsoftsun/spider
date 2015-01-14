21db45d3fa96ab8a6549d3203e94d607c8c29995
zhaopengcheng <zhaopcprayer@gmail.com>
2015-01-14 15:21:28 +0800

    meituan

--- src/tuangou/meituan.js
+++ src/tuangou/meituan.js
@@ -1,90 +1,205 @@
 var fs = require('fs')
-var cheerio = require("cheerio")
 var helper = require('../../helpers/webhelper.js')
+var cheerio = require('cheerio')
 
-function Mt(){
-    this.resultDir = "../../result/";
-    this.dataDir = "../../appdata/";
-    this.resultFile = "meituan.txt";
-    this.cityFile = "meituan_city.txt";
-    this.categoryFile = "meituan_categories.txt";
-    this.cities = [];
-    
-    this.categories = [];
-    this.taskQueue = [];
-    this.interval = [0,500];
-    this.doneItems = {};
+function Meituan() {
+    this.dataDir = '../../appdata/';
+    this.resultDir = '../../result/';
+    this.citys = [];
+    this.cityFile = 'meituan.city.txt';
+    this.categorys = [];
+    this.categoryFile = 'meituan.category.txt';
+    this.resultFile = 'meituan.txt';
+    this.breakpointDir = './';
+    this.breakpointFile = 'meituan.breakpoint';
+    this.breakpoint = '';
 }
 
-Mt.prototype.init = function(){
-    var doneCount = 0;
+Meituan.prototype.init = function(){
+    var lines = fs.readFileSync(this.dataDir+this.cityFile).toString().split('\n');
+    for(var i = 0, l = lines.length; i < l; i++) {
+        if(lines[i]) {
+            var vals = lines[i].split(',');
+            this.citys.push({"cityName":vals[0],"cityUrl":vals[1]});
+        }
+    }
 
-    var args = process.argv.slice(2);
-    if(args.length>0){
-	this.startIdx = args[0];
+    var lines = fs.readFileSync(this.dataDir+this.categoryFile).toString().split('\n');
+    for(var i = 0, l = lines.length; i < l; i++) {
+        if(lines[i]) {
+            var vals = lines[i].split(',');
+            this.categorys.push({"cat1_name":vals[0],"cat2_name":vals[1],"cat_ename":vals[2]});
+        }
     }
-    if(args.length>1){
-	this.count = args[1];
+
+    if(fs.existsSync(this.breakpointDir+this.breakpointFile)) {
+        var line = fs.readFileSync(this.breakpointDir+this.breakpointFile).toString();
+        var tmp = line.split(',');
+        this.breakpoint = [Number(tmp[0]), Number(tmp[1])];
     }
-    
-    this.cities = fs.readFileSync(this.dataDir+this.cityFile).toString().split('\n').filter(function(line){
-	return line;
-    }).map(function(line){
-	var vals = line.split(',');
-	return {id:vals[0],name:vals[1],code:vals[2]};
-    });
-    this.categories = fs.readFileSync(this.dataDir+this.categoryFile).toString().split('\n').filter(function(line){
-	return line;
-    }).map(function(line){
-	var vals = line.split(',');
-	return {id:vals[0],name:vals[1]};
-    });
-    if(!fs.existsSync(this.resultDir+this.resultFile)){
-	console.log("[DONE COUNT] %d",doneCount);
-	return;
+
+    this.tasks = [];
+    for(var i = 0; i< this.citys.length; i++) {
+        var city = this.citys[i];
+        for(var j = 0; j < this.categorys.length; j++) {
+            var category = this.categorys[j];
+            var taskNum = i * this.categorys.length + j;
+            var tmp = {"taskNum":taskNum,"cityName":city.cityName,"cityUrl":city.cityUrl,"cat1_name":category.cat1_name,"cat2_name":category.cat2_name,"cat_ename":category.cat_ename};
+            this.tasks.push(tmp);
+        }
     }
-    fs.readFileSync(this.resultDir+this.resultFile).toString().split('\n').map(function(line){
-	if(line){
-	    return line.split(',')[3];
-	}
-	return "";
-    }).reduce(function(pre,cur){
-	pre[cur]=true;
-	++doneCount;
-	return pre;
-    },this.doneItems);
-    //console.log(this.doneItems);
-    console.log("[DONE COUNT] %d",doneCount);
-    console.log("[CITIES] %d",this.cities.length);
-    console.log("[CATEGORIES] %d",this.categories.length);
+    var arguments = process.argv.splice(2);
+    var start = Number(arguments[0]);
+    var len = Number(arguments[1]);
+    //前闭后开区间
+    this.tasks = this.tasks.slice(this.breakpoint[0],start+len);
+    console.log("[INFO] task count: %d",this.tasks.length);
 }
 
-Mt.prototype.start = function(){
+Meituan.prototype.start = function(){
     this.init();
-    for(var i=0;i<this.cities.length;i++){
-	for(var j=0;j<this.categories.length;j++){
-	    if(this.cities[i] && this.categories[j])
-		this.taskQueue.push({city:this.cities[i],cate:this.categories[j]});
-	}
-    }
-    this.startIdx = Number(this.startIdx) || 0;
-    this.count = Number(this.count) || this.taskQueue.length;
-    this.taskQueue = this.taskQueue.slice(this.startIdx,this.startIdx+this.count);
-    console.log("[TASKS] %d",this.taskQueue.length);
-    
     this.wgetList();
 }
-Mt.prototype.wgetList = function(t){
 
+Meituan.prototype.wgetList = function(t){
+    if(!t) {
+        if(this.tasks.length==0){
+            console.log("job done.");
+            return;
+        }
+        t = this.tasks.shift();
+        if(this.breakpoint) {
+            t.pn = this.breakpoint[1];
+            this.breakpoint = 0;
+        } else {
+            t.pn = 1;
+        }
+        console.log('task left: %d', this.tasks.length);
+    }
+
+    var opt = new helper.basic_options(t.cityUrl.replace('http://',''), "/category/"+t.cat_ename+"/all/page"+t.pn);
+    opt.headers['User-Agent'] = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)";
+    opt.headers['Accept'] = "text/html, application/xhtml+xml, */*";
+    opt.headers['Referer'] = t.cityUrl+'/';
+    opt.headers['Accept-Encoding'] = "gzip, deflate";
+    opt.headers['Accept-Language'] = "zh-CN";
+
+    opt.agent = false;
+    console.log("[GET ] %s, %s, %s, %d",t.cityName,t.cat1_name,t.cat2_name,t.pn);
+    fs.writeFileSync(that.breakpointDir+that.breakpointFile,t.taskNum.toString()+','+t.pn.toString()+'\n');
+    helper.request_data(opt,null,function(data,args,res){
+    	that.wgetDetail(data,args,res);
+    },t);
 }
 
-Mt.prototype.wgetDetail = function(task){
+Meituan.prototype.wgetDetail = function(data,args,res) {
+    t = args[0];
+    if(!data) {
+        console.log("page data empty");
+        t.pn++;
+        if(t.pn <= 200) {
+            setTimeout(function () {
+                that.wgetList(t);
+            }, (Math.random() * 1 + 2) * 1000);
+        } else {
+            setTimeout(function () {
+                that.wgetList();
+            }, (Math.random() * 1 + 2) * 1000);
+        }
+    } else {
+        var $ = cheerio.load(data);
+        var if_empty = $("div.filter-label-list.filter-section.category-filter-wrapper").length;
+        if(if_empty) {
+            console.log("category empty");
+            setTimeout(function () {
+                that.wgetList();
+            }, (Math.random() * 2 + 1) * 1000);
+        } else {
+            var next_page = $("div.paginator-wrapper li.next").length;
+            if(next_page) {
+                t.exist_next_page = 1;
+            } else {
+                t.exist_next_page = 0;
+            }
+            var script = $("script:contains('M.Env[\"data-conf\"]')").text();
+            var json_string = script.substring(21, script.length-1);
+            var json_data = JSON.parse(json_string);
+            var asyncPageviewData = json_data['asyncPageviewData'];
+            if(asyncPageviewData.length == 0) {
+                console.log("list empty");
+                setTimeout(function () {
+                    that.wgetList();
+                }, (Math.random() * 2 + 1) * 1000);
+            } else {
+                var params = {};
+                var acms = asyncPageviewData['acms'];
+                if(acms) {
+                    params['params'] = '{"mteventParams":'+$("div#content").attr('data-mteventnd')+'}';
+                    params['geotype'] = 'all';
+                    params['areaid'] = 'all';
+                    params['offset'] = 0;
+                    params['dealids'] = asyncPageviewData['deals'];
+                    params['acms'] = asyncPageviewData['acms'].join();
+                    post_data = require('querystring').stringify(params);
 
+                    var opt = new helper.basic_options(t.cityUrl.replace("http://",''),"/index/deallist","POST",0,1);
+                    opt.agent = false;
+                    helper.request_data(opt,post_data,function(data1,args,res){
+                        that.processData(data1,args,res);
+                    },t);
+                } else {
+                    console.log("acms empty");
+                    setTimeout(function () {
+                        that.wgetList();
+                    }, (Math.random() * 2 + 1) * 1000);
+                }
+            }
+        }
+    }
 }
 
-Mt.prototype.processDetail = function(data,args){
-
+Meituan.prototype.processData = function(data,args,res) {
+    t = args[0];
+    if(!data) {
+        console.log("ajax data empty");
+        if(t.exist_next_page) {
+            t.pn++;
+            setTimeout(function () {
+                that.wgetList(t);
+            }, (Math.random() * 2 + 1) * 1000);
+        } else {
+            setTimeout(function () {
+                that.wgetList();
+            }, (Math.random() * 1 + 2) * 1000);
+        }
+    } else {
+        if('data' in data) {
+            var $ = cheerio.load(data['data']);
+            $("div.deal-tile").each(function() {
+                var title = $("h3 span.xtitle",this).text().replace(/[\n\r,，]/g,";");
+                var description = $("h3 span.short-title",this).text().replace(/[\n\r,，]/g,";");
+                var url = $("h3 a",this).attr("href");
+                var price = $("p.deal-tile__detail strong",this).text();
+                var sale_num = $("div.deal-tile__extra p.extra-inner span.sales strong.num",this).text();
+                var rate_num = $("div.deal-tile__extra p.extra-inner a.rate-info span.rate-info__count",this).text().replace("人评价",'')||0;
+                var item = [title,description,price,sale_num,rate_num,url,"\n"].join();
+                if(title)
+                    fs.appendFileSync(that.resultDir+that.resultFile,item);
+            });
+        }
+        if(t.exist_next_page) {
+            t.pn++;
+            setTimeout(function () {
+                that.wgetList(t);
+            }, (Math.random() * 2 + 1) * 1000);
+        } else {
+            setTimeout(function () {
+                that.wgetList();
+            }, (Math.random() * 1 + 2) * 1000);
+        }
+    }
 }
 
-var that = new Mt();
+var instance = new Meituan();
+var that = instance;
 that.start();
