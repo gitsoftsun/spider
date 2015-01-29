689f4e62c72a72d42f7bd016240a8718d743f62b
unknown <User19@user19.bdachina.com>
2015-01-29 10:22:50 +0800

    add py

---
+++ python/shangou.huimaiche.py
@@ -0,0 +1,47 @@
+#coding:utf8
+
+import urllib2
+import time
+import json
+from pyquery import PyQuery as pq
+
+# the funtion for get every city url
+# URL = 'http://beijing.huimaiche.com/shangou/list'
+# def get_cityUrls():
+#     urls = []
+#     html = urllib2.urlopen(URL).read()
+#     htmlPq = pq(html)(".change-city li a");
+#     fw = open('urls.txt', 'w+')
+#     for i in range(0, len(htmlPq)):
+#         url = htmlPq.eq(i).attr['href']
+#         cname = htmlPq.eq(i).text()
+#         entity = cname + ',' + url + '\n'
+#         fw.write(entity.encode('utf8'))
+#     fw.close()
+
+def get_info(urls):
+    fw = open('info.txt', 'w+')
+    for url in urls:
+        js_text = urllib2.urlopen(url).read()
+        json_string = js_text[int(str(js_text).index("_carsCb(["))+8:len(js_text)-4]
+        
+        data = json.loads(json_string)
+        # print data
+        for car_info in data:
+                entity = car_info['SaleCity'].encode('utf8') +"\t"+car_info['CarYear'].encode('utf8') +"\t"+car_info['CsName'].encode('utf8') +"\t"+car_info['CarName'].encode('utf8') +"\t"+str(car_info['Stock']) +"\t"+car_info['RapidPrice'].encode('utf8') +"\t"+car_info['ReferPrice'].encode('utf8') +"\t"+car_info['CurTime'].encode('utf8') +"\t"+str(car_info['TimeType']) +"\t"+car_info['MaxSave'].encode('utf8') +"\t"+str(car_info['Buyer']) +"\n"
+                fw.write(entity)
+        time.sleep(3)
+    #fw.close()
+
+def testStr(var):
+    pass
+def main():
+    bjurl = 'http://ajax.huimaiche.com/RapidIndexCars.ashx?v=1422431019054&callback=jQuery11100478390209376812_1422431017426&ccode=201&top=1000&all=1&callback=%24.brand_oneprice.loaders.rapidCarskLoader._carsCb&_=1422431017427'
+    gzurl = 'http://ajax.huimaiche.com/RapidIndexCars.ashx?v=1422438067183&callback=jQuery11100259428184479475_1422438064293&ccode=501&top=1000&all=1&callback=%24.brand_oneprice.loaders.rapidCarskLoader._carsCb&_=1422438064295'
+    whurl = 'http://ajax.huimaiche.com/RapidIndexCars.ashx?v=1422438112250&callback=jQuery111005999405172187835_1422438111324&ccode=1201&top=1000&all=1&callback=%24.brand_oneprice.loaders.rapidCarskLoader._carsCb&_=1422438111326'
+    cdurl ='http://ajax.huimaiche.com/RapidIndexCars.ashx?v=1422438137366&callback=jQuery111009785614341963083_1422438136155&ccode=2501&top=1000&all=1&callback=%24.brand_oneprice.loaders.rapidCarskLoader._carsCb&_=1422438136156'
+    urls = [bjurl, gzurl, whurl, cdurl]
+    get_info(urls)
+
+if __name__ == "__main__":
+    main()
